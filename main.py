bae91c802cacedafa09395ca1447719cd850f1a7
Augustge <augustgeelmuyden92@gmail.com>
2020-03-27 11:50:45 +0000

    working static image

--- main.py
+++ main.py
@@ -10,47 +10,70 @@ from kivy.config import Config
 from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.graphics import Rectangle, Color
+from kivy.core.image import Image as CoreImage
+from kivy.uix.image import Image
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 
 import requests
 import json
 import urllib
-import ssl
-
-
 import geocoder
+import io
+import PIL
+import urllib
+# from PIL import Image, ImageDraw, ImageFont
+
+
 
 
 class LocalInfo(BoxLayout):
-    # lat,lng =
-    APIKEY = "AIzaSyABY9PJX_0ScAy6Sdtv3_bMa5qmOpL_ZBc"
-    queryString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius={radius}&type={type}&key={APIKEY}{pagetoken}"
+    orientation = "vertical"
+    lat,lng = 0,0
+    APIKEY      = "AIzaSyABY9PJX_0ScAy6Sdtv3_bMa5qmOpL_ZBc"
+    mapboxKEY   = "pk.eyJ1IjoiYXVndXN0Z2UiLCJhIjoiY2s4YTJyNHYwMGNoNjNscWw5bHc5cGhoeiJ9.AxRUBI4Qd_Zgv-lyPnv2SA"
+    queryString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{long}&radius={radius}&type={type}&key={APIKEY}{pagetoken}"
+    mapboxQs    = "https://api.mapbox.com/styles/v1/mapbox/{style}/static/{lon},{lat},{zoom}/{w}x{h}{retina}?access_token={token}&attribution={attribution}&logo=false"
+    staticMapQuery = "http://www.mapquestapi.com/staticmap/v4/getmap"
+
 
     def __init__(self, **kwargs):
         super(LocalInfo, self).__init__(**kwargs)
         self.info = Label(text="tototot")
         self.add_widget(self.info)
         self.getPosition()
-        with self.canvas:
-            Color(.234, .456, .678, .8)  # set the colour
-            self.rect = Rectangle(size =(100,100))
+        self.getLocalInfo()
+        self.getStaticMapAround()
+        self.info.text += "\n"
+        for place in self.localPlaces:
+            self.info.text += place["name"] + "\n"
+        # with self.canvas:
+        #     Color(.234, .456, .678, .8)  # set the colour
+        #     self.rect = Rectangle(size =(100,100))
 
     def getPosition(self):
         g = geocoder.ip('me')
-        self.lat, self.lng  = g.latlng
-        self.info.text = "lat:%.4f long:%.4f"%(self.lat, self.lng)
+        self.lat, self.long  = g.latlng
+        self.info.text = "lat:%.4f long:%.4f"%(self.lat, self.long)
 
-    def getLocalInfo(self,lat,lng,type="restaurant",radius=300,pagetoken=None):
-        url = self.queryString.format(
-                        lat=self.lat,lng=self.lng,radius=radius,type=type,APIKEY=self.APIKEY,pagetoken="&pagetoken="+pagetoken if pagetoken else "")
+    def getLocalInfo(self,type="restaurant",radius=300,pagetoken=None):
+        pagetoken = "&pagetoken="+pagetoken if pagetoken else ""
+        url = self.queryString.format(  lat=self.lat,long=self.long,
+                                        radius=radius,type=type,
+                                        APIKEY=self.APIKEY,
+                                        pagetoken=pagetoken)
         response = requests.get(url)
         res = json.loads(response.text)
-        for result in res["results"]:
-            print(result["name"])
-            for key in result:
-              print(" --",key,result[key])
+        self.localPlaces = res["results"]
 
+    def getStaticMapAround(self,zoom=14):
+        url = self.mapboxQs.format(style="streets-v10",lat=self.lat,lon=self.long,zoom=zoom,w=400,h=400,retina="",token=self.mapboxKEY,attribution="true")
+        with urllib.request.urlopen(url) as response:
+            data = io.BytesIO(response.read())
+            data.seek(0)
+            self.img = CoreImage(data, ext='png')
+            self.IMG = Image(texture=self.img.texture)
+            self.add_widget(self.IMG)
 
 
 class Main(App):
@@ -59,7 +82,7 @@ class Main(App):
 
     def constructBox(self):
         self.box   = BoxLayout(orientation='vertical')
-        self.box.add_widget(Label(text="TATA"))
+        self.box.add_widget(Label(text="TATA",size_hint=(1,0.05)))
         self.mapwindow = LocalInfo()
         self.box.add_widget(self.mapwindow)
 
